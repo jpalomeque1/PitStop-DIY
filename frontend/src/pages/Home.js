ccd660cfc567a3342fa808eb0f1af140422f4e00
juan <jup017@ensign.edu>
2025-02-05 15:17:20 -0700

    WIP: Save local changes before rebase

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
             <div className="main-content">
                 <HomeBody />
             </div>
-            <Footer className="footer" />
+
         </div>
     );
 };
